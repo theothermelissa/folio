be53682ac606f1e00b5bd61c00d17f74093deb3e
Melissa Morgan <melissapearlmorgan@gmail.com>
2023-07-03 14:03:46 -0500

    wip: auth!!

--- components/claim-feed.tsx
+++ components/claim-feed.tsx
@@ -2,12 +2,11 @@ import { Button, Flex, Input, Spinner } from "@chakra-ui/react";
 import styled from "@emotion/styled";
 import { useRouter } from "next/router";
 import { ChangeEvent, useEffect, useState } from "react";
-import { sendOTP } from "../lib/auth";
+import { handleOTPInput, sendOTP } from "../lib/auth";
 
 const Wrapper = styled(Flex)`
   flex-direction: row;
   justify-content: flex-end;
-  //   width: 33%;
   height: 100%;
   align-items: center;
   margin-right: 24px;
@@ -18,83 +17,101 @@ const CodeInput = styled(Input)`
   width: 250px;
 `;
 
-const handleCodeSubmit = async (code: string) => {
-  // check if the code is correct
-};
-
 export const ClaimFeed = () => {
-  return <Button onClick={() => sendOTP("+14056403957")}>Claim</Button>;
-  // const router = useRouter();
-  // const { subdomain } = router.query;
-  // console.log("router.query in claim-feed: ", router.query);
+  const router = useRouter();
+  const { subdomain } = router.query;
+  //   console.log("router.query in claim-feed: ", router.query);
+  const [codeRequested, setCodeRequested] = useState(false);
+  const [showCodeInput, setShowCodeInput] = useState(false);
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  // //   const [codeRequested, setCodeRequested] = useState(false);
-  // const [showCodeInput, setShowCodeInput] = useState(false);
-  // const [code, setCode] = useState("");
-  // const [submitted, setSubmitted] = useState(false);
-  // const [otpMetadata, setOtpMetadata] = useState({} as any);
+  // TODO mpm: refactor this to be handled in the handleChangeCode function
+  useEffect(() => {
+    if (code.length > 5) {
+      console.log("code length reached");
+      setLoading(true);
+      setShowCodeInput(false);
+      handleSubmitCode();
+    }
+  }, [code]);
 
-  // useEffect(() => {
-  //   if (code.length > 5) {
-  //     console.log("code length reached");
-  //     setCode("");
-  //     setSubmitted(true);
-  //     setShowCodeInput(false);
-  //     handleSubmitCode();
-  //   }
-  // }, [code]);
-  // //   const [emailRequested, setEmailRequested] = useState(false);
+  const handleSubmitCode = async () => {
+    handleOTPInput(code).then(() => {
+      setLoading(false);
+      setCode("");
+    });
+    //   const loadingOtp = { userInputCode: code, ...otpMetadata };
+    //   console.log("loadingOtp to put into body: ", {
+    //     userInputCode: code,
+    //     ...otpMetadata,
+    //   });
+    //   const params: RequestInit = {
+    //     headers: {
+    //       "x-subdomain": subdomain.toString(),
+    //     },
+    //     method: "POST",
+    //     body: JSON.stringify(loadingOtp),
+    //   };
+    //   const authResponse = await fetch("/api/account/claim", params).then((res) =>
+    //     res.json()
+    //   );
+    //   console.log("authResponse: ", authResponse);
+    //   if (authResponse.status === 200) {
+    //     alert("Successfully claimed feed!");
+    //   }
+  };
 
-  // const handleSubmitCode = async () => {
-  //   const submittedOtp = { userInputCode: code, ...otpMetadata };
-  //   console.log("submittedOtp to put into body: ", {
-  //     userInputCode: code,
-  //     ...otpMetadata,
-  //   });
-  //   const params: RequestInit = {
-  //     headers: {
-  //       "x-subdomain": subdomain.toString(),
-  //     },
-  //     method: "POST",
-  //     body: JSON.stringify(submittedOtp),
-  //   };
-  //   const authResponse = await fetch("/api/account/claim", params).then((res) =>
-  //     res.json()
-  //   );
-  //   console.log("authResponse: ", authResponse);
-  //   if (authResponse.status === 200) {
-  //     alert("Successfully claimed feed!");
-  //   }
-  // };
+  async function onOtpRequest() {
+    setLoading(true);
+    const result = await sendOTP("+12567106534");
+    console.log("result of sendOTP: ", result);
+    if (result && result.status === "OK") {
+      setLoading(false);
+      setShowCodeInput(true);
+    }
+  }
 
-  // const handleRequestOtp = async () => {
-  //   const requestOtpMetadata = await fetch("/api/account/claim", {
-  //     method: "POST",
-  //     headers: {
-  //       "x-subdomain": subdomain.toString(),
-  //     },
-  //   }).then((res) => res.json());
-  //   console.log("requestOtpMetadata: ", requestOtpMetadata);
-  //   setSubmitted(false);
-  //   setShowCodeInput(!showCodeInput);
-  //   setOtpMetadata({ ...requestOtpMetadata });
-  // };
-
-  // return (
-  //   <Wrapper>
-  //     {showCodeInput ? (
-  //       <CodeInput
-  //         type="text"
-  //         placeholder="Submit code"
-  //         onChange={(event: ChangeEvent<HTMLInputElement>) =>
-  //           setCode(event.target.value)
-  //         }
-  //       />
-  //     ) : (
-  //       <Button onClick={handleRequestOtp} colorScheme="green">
-  //         {submitted ? <Spinner /> : "Claim this feed"}
-  //       </Button>
-  //     )}
-  //   </Wrapper>
-  // );
+  return (
+    <Wrapper>
+      {showCodeInput ? (
+        <CodeInput
+          type="text"
+          placeholder="Submit code"
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setCode(event.target.value)
+          }
+        />
+      ) : (
+        <Button onClick={onOtpRequest} colorScheme="green">
+          {loading ? <Spinner /> : "Claim this feed"}
+        </Button>
+      )}
+    </Wrapper>
+  );
 };
+
+// export const ClaimFeed = () => {
+//   const [otpMetadata, setOtpMetadata] = useState({} as any);
+
+//   //   const [emailRequested, setEmailRequested] = useState(false);
+
+//
+
+//   const handleRequestOtp = async () => {
+//     const requestOtpMetadata = await fetch("/api/account/claim", {
+//       method: "POST",
+//       headers: {
+//         "x-subdomain": subdomain.toString(),
+//       },
+//     }).then((res) => res.json());
+//     console.log("requestOtpMetadata: ", requestOtpMetadata);
+//     setLoading(false);
+//     setShowCodeInput(!showCodeInput);
+//     setOtpMetadata({ ...requestOtpMetadata });
+//   };
+
+//   return (
+//
+//   );
+// };
