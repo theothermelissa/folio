fe3623b6ecce575a49c710d6cea38248449b60db
Melissa Morgan <melissapearlmorgan@gmail.com>
2023-07-06 16:48:01 -0500

    fix: use dynamic cors matcher

--- pages/api/auth/[[...path]].ts
+++ pages/api/auth/[[...path]].ts
@@ -8,14 +8,17 @@ import NextCors from "nextjs-cors";
 
 supertokens.init(backendConfig());
 
+const { NEXT_PUBLIC_BASE_URL_PATH } = process.env;
+
 export default async function superTokens(
   req: NextApiRequest & Request,
   res: NextApiResponse & Response
 ) {
+  const originRegEx = new RegExp("." + NEXT_PUBLIC_BASE_URL_PATH + "$");
   // NOTE: We need CORS only if we are querying the APIs from a different origin
   await NextCors(req, res, {
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: /.localhost:3000$/,
+    origin: originRegEx,
     credentials: true,
     allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
   });
